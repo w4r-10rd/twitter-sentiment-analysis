e0f11cff515632eff5b75c721d2ce6f227864c40
w4r-10rd <arijitnath52@gmail.com>
2024-12-19 19:27:32 +0530

    legacy push

---
+++ enltk.py
@@ -0,0 +1,5 @@
+import nltk
+nltk.download('punkt')        # Required for tokenization
+nltk.download('stopwords')    # Required for removing stopwords
+nltk.download('wordnet')      # Required for lemmatization
+nltk.download('omw-1.4')      # WordNet enhancements (optional)
